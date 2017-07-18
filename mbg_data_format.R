62dc9f4c3f5581c4f8ec252086ebbbc5464d3ec8
IHME\stearns7 <stearns7@uw.edu>
2017-07-18 16:53:07 -0700

    go

--- mbg_data_format.R
+++ mbg_data_format.R
@@ -12,8 +12,8 @@ my_lib <- ("C:/Users/stearns7/Documents/R/win-library/3.3")
 pacman::p_load(fields, data.table, magrittr, stringr, reshape2, ggplot2, readbulk, ggmap, devtools, scale,
                dplyr, Amelia, plyr, rgdal, raster,  seegSDM, seegMBG, sp, rgeos, maptools, rgeos, rgdal)
 
-latest_dir <- ('C:/Users/stearns7/OneDrive - UW Office 365/ntds/LF/data/processed_data/all')
-save_dir <- ('C:/Users/stearns7/OneDrive - UW Office 365/ntds/LF/data/mbg/formatted')
+latest_dir <- ('C:/Users/stearns7/OneDrive - UW Office 365/ntds/LF/data/processed_data/all/')
+save_dir <- ('C:/Users/stearns7/OneDrive - UW Office 365/ntds/LF/data/mbg/formatted/')
 data_dir <- ('C:/Users/stearns7/OneDrive - UW Office 365/ntds/LF/data/processed_data/latest/')
 
 #------------------------------------------ load data ----------------------------------------------------
@@ -32,7 +32,7 @@ data_dir <- ('C:/Users/stearns7/OneDrive - UW Office 365/ntds/LF/data/processed_
 # latest clean lf dataset (as of 6/28/2017, last edited on 6/28/2017 at 14:17, assuming by Kevin)
 #all <- fread(paste0(data_dir, 'lf_data_fin.csv'), stringsAsFactors = F)
 
-# latest clean lf dataset (as of 7/18/2017, last edited on 7/12/2017 at 11:13(not by me))
+# latest clean lf dataset (as of 7/18/2017, last edited on 7/12/2017 at 11:13(not by me)) -- 9128 rows
 all <- fread(paste0(data_dir, 'lf_data_fin.csv'), stringsAsFactors = F)
 #all <- all[,-c('V1')]
 
@@ -41,10 +41,10 @@ all <- fread(paste0(data_dir, 'lf_data_fin.csv'), stringsAsFactors = F)
 a <- copy(all)
 
 no_prev <- a[prev_dx=='',]
-write.csv(no_prev, file = paste0(data_dir, 'no_prev.csv'))
+#write.csv(no_prev, file = paste0(data_dir, 'no_prev_7.18.csv'))
 
 a1 <- a[prev_dx != '',] #9015 with latest (6/28)
-a1 <- a1[,-c(69:77), with=FALSE]
+a1 <- a1[,-c(68:76), with=FALSE]
 
 # ---------------------------------------- getting single set of prev measures --------------------------
 b <- copy(a1)
@@ -87,7 +87,7 @@ b3 = sapply(sort(unique(b2$dx_to_use)), function(i) {
 
 #save summary of what diagnostics used in this dataset
 summ_dx <- table(b2$dx_to_use)
-#write.csv(summ_dx, file=paste0(data_dir, 'summ_dx_used.csv'))
+#write.csv(summ_dx, file=paste0(data_dir, 'summary/summ_dx_used_7.18.csv'))
 
 b2[,dx:=dx_to_use]
 
@@ -115,7 +115,7 @@ c3 <- c2[no_year ==0 & no_denom ==0,]
 
 #place those in need of attention aside
 c4 <- c2[no_year == 1 | no_denom == 1,]
-#write.csv(c4, file=paste0(data_dir, 'no_year.csv'))
+#write.csv(c4, file=paste0(data_dir, '/data_needs_review/no_year_7.18.csv'))
 
 # -------------------------------------- assigning iso3 codes ------------------------------------------
 d <- copy(c3)
@@ -123,227 +123,69 @@ d <- copy(c3)
 d[,country:=adm0]
 
 #need iso3 codes for R shiny and data plots
-iso3 <- fread(file='C:/Users/stearns7/OneDrive - UW Office 365/spatial_data/mbg_gaul_iso3_key.csv', stringsAsFactors = F)
+iso3 <- fread(file='C:/Users/stearns7/OneDrive - UW Office 365/spatial_data/mbg_gaul_iso3_key_lf_adapted.csv', stringsAsFactors = F)
 
 #removing accent from cote d'ivoire
-iso3 <- iso3[official_name=="Cote D'Ivoire", official_name:="Cote d'Ivoire"]
-iso3 <- iso3[official_name == 'United Republic of Tanzania', official_name:="Tanzania"]
-iso3 <- iso3[official_name == 'Venezuela (Bolivarian Republic of)', official_name:="Venezuela"]
-iso3 <- iso3[official_name == 'Viet Nam', official_name:="Vietnam"]
-iso3 <- iso3[official_name == 'Wallis and Futuna Islands', official_name:="Wallis and Futuna"]
+#iso3 <- iso3[official_name=="Cote D'Ivoire", official_name:="Cote d'Ivoire"]
+#iso3 <- iso3[official_name == 'United Republic of Tanzania', official_name:="Tanzania"]
+#iso3 <- iso3[official_name == 'Venezuela (Bolivarian Republic of)', official_name:="Venezuela"]
+#iso3 <- iso3[official_name == 'Viet Nam', official_name:="Vietnam"]
+#iso3 <- iso3[official_name == 'Wallis and Futuna Islands', official_name:="Wallis and Futuna"]
 
-write.csv(iso3, file = 'C:/Users/stearns7/OneDrive - UW Office 365/spatial_data/mbg_gaul_iso3_key_lf_adapted.csv')
+#write.csv(iso3, file = 'C:/Users/stearns7/OneDrive - UW Office 365/spatial_data/mbg_gaul_iso3_key_lf_adapted.csv')
 
 #match to data
 d1 <- merge(d, iso3, by.x = 'adm0', by.y='official_name')
 
+#make iso3 code country var
+d2 <- d1[, country:=iso3]
+
 # ------------------------------------ deal with sampling (temporary) ----------------------------------
-d2 <- copy(d1)
-d2[point==0,sampling:=99]
-d2[point==0 & sampling_prod==0, sampling:=0]
-d2[point==0 & sampling_prod==1, sampling:=1]
-d2[point==0 & sampling==99, sampling:=1]
+d3 <- copy(d2)
+d3[point==0,sampling:=99]
+d3[point==0 & sampling_prod==0, sampling:=0]
+d3[point==0 & sampling_prod==1, sampling:=1]
+d3[point==0 & sampling==99, sampling:=1]
+
+
+#save latest
+write.csv(d3, file=paste0(data_dir, 'lf_data_pre_mbg_trim&poly_rev_drop_7.18.csv'))
+
+#subset to polygon rows that have been reviewed by Liz
+d4 <- d3[poly_rev != 1 | is.na(poly_rev),]
+
+# ------------------------------------ drop rows with N < 20  ----------------------------------
+
+d5 <- copy(d4)
+
+#rows with N < 20
+lt20 <- d5[N < 20,]
+#write.csv(lt20, file=paste0(data_dir, 'data_needs_review/n_less_than_20_7.18.csv'))
+
+#dropping the rows with N < 20
+d6 <- d5[N >= 20,]
+
+#Add lf_uid
+row.names(d6) <- 1:nrow(d6)
+d6$lf_uid <- 1:nrow(d6)
+
+#setting source
+d6[,source:=data_source]
 
 # -------------------------------------- trimming fields -------------------------------------------------
-e <- copy(d2)
+e <- copy(d6)
 
 e1 <- e[,c('lf_uid', 'source', 'original_year', 'country', 
            'point', 'latitude', 'longitude', 'shapefile', 'location_code', 'sampling', 'N', 
            'lf_pos', 'lf_prev', 'weight', 'cluster_id', 'year'), with=F]
 
-write.csv(e1,file = paste0(data_dir,'lf_pos_all.csv'))
+write.csv(e1,file = paste0(save_dir,'lf_pos_all.csv'))
+
+#points only
+e2 <- e1[point==1,]
+e2 <- e2[,-c('point', 'shapefile', 'location_code', 'sampling'), with=F]
+
+write.csv(e2,file = paste0(save_dir,'lf_pos_pts_only.csv'))
 
 
 
-#scrap
-
-
-
-# ---------------------------------------- subset to prev only rows -- no sequela --------------------------
-a <- copy(all)
-
-no_prev <- a[prev_dx=='',]
-#write.csv(no_prev, file = paste0(latest_dir, '/no_prev.csv'))
-
-a1 <- a[prev_dx != '',] #8914 with latest (6/1)
-
-# ---------------------------------------- getting single set of prev measures --------------------------
-b <- copy(a1)
-
-b[,N:=NA]
-b[,lf_pos:=NA]
-b[,lf_prev:=NA]
-
-#mf rows
-b_mf <- b[grep('mf', b$prev_dx),] #3198 rows
-b_mf[,N:=pop_mf]
-b_mf[,lf_pos:=np_mf]
-b_mf[,lf_prev:=prev_mf]
-
-#set aside rows that report prev only -- 22 rows
-b_mf_prev_only <- b_mf[is.na(N),]
-#write.csv(b_mf_prev_only, file = paste0(latest_dir, '/prev_only_mf.csv'))
-
-b_mf_fin <- b_mf[!is.na(N),] #3176 rows
-
-#ict rows
-mf_uids <- b_mf[['uid_from_source']]
-b_next <- b[!uid_from_source %in% mf_uids,]
-
-b_ict <- b_next[grep('ict', b_next$prev_dx),] #5421 rows
-b_ict[,N:=pop_ict]
-b_ict[,lf_pos:=np_ict]
-b_ict[,lf_prev := prev_ict]
-
-b_ict_prev_only <- b_ict[is.na(N),] #35
-#write.csv(b_ict_prev_only, file = paste0(latest_dir, '/prev_only_ict.csv'))
-
-b_ict_fin <- b_ict[!is.na(N),] #5386
-
-#elisa rows
-ict_uids <- b_ict[['uid_from_source']]
-
-b_next2 <- b_next[!uid_from_source %in% ict_uids,]
-
-b_elisa <- b_next2[grep('elisa', b_next2$prev_dx),] #166 rows
-b_elisa[,N:=pop_elisa]
-b_elisa[,lf_pos:=np_elisa]
-b_elisa[,lf_prev:=prev_elisa]
-
-#br rows
-elisa_uids <- b_elisa[['uid_from_source']]
-
-b_next3 <- b_next2[!uid_from_source %in% elisa_uids,]
-
-b_br <- b_next3[grep('br', b_next3$prev_dx),] #21 rows
-b_br[,N:=pop_br]
-b_br[,lf_pos:=np_br]
-b_br[,lf_prev:=prev_br]
-
-#pcr rows
-br_uids <- b_br[['uid_from_source']]
-
-b_next4 <- b_next3[!uid_from_source %in% br_uids,]
-
-b_pcr <- b_next4[grep('pcr', b_next4$prev_dx),] #3 rows
-b_pcr[,N:=pop_pcr]
-b_pcr[,lf_pos:=np_pcr]
-b_pcr[,lf_prev:=prev_pcr]
-
-#other rows
-pcr_uids <- b_pcr[['uid_from_source']]
-
-b_next5 <- b_next4[!uid_from_source %in% pcr_uids,]
-
-#all that remain are either 'other' or report density only
-write.csv(b_next5, file = paste0(latest_dir, '/other_dens_only.csv'))
-
-#bind all back together
-mbg_list <- list(b_mf_fin, b_ict_fin, b_elisa, b_br, b_pcr)
-b2 <- rbindlist(mbg_list, use.names = T) #8752 rows
-#write.csv(b2, file = paste0(latest_dir, '/mbg_prevs_populated_6.2.csv'))
-
-# ---------------------------------------- adding necessary fields ---------------------------------------
-c <- copy(b2)
-
-c[,weight:=1]
-c[,cluster_id:=1]
-
-c1 <- c[,original_year:=NA]
-c1[,original_year:=year_start]
-
-c1[,year:=year_start]
-c1[year_start <=2000, year:=2000]
-
-c1[,lf_uid:=rownames(c1)]
-
-c1[,source:=data_source]
-
-c1[,country:=adm0]
-#write.csv(c1, file = paste0(save_dir, '/all_mbg_nec_fields_plus.6.2.csv'))
-
-#c1 <- fread(file = paste0(save_dir, '/all_mbg_nec_fields_plus.5.22.csv'), stringsAsFactors = F)
-#c1 <- c1[,-c('V1')]
-
-#drop rows with N=0, & year start =0
-c2 <- c1[,no_year:=0]
-c2[year_start==0, no_year:=1]
-c2[is.na(year_start), no_year:=1]
-c2[,no_denom:=0]
-c2[N==0, no_denom:=1]
-
-c3 <- c2[no_year ==0 & no_denom ==0,]
-# c3[adm0_code==90, adm0 := 'Gambia']
-# c3[adm0_code == 257, adm0 := 'Tanzania']
-# c3[adm0_code == 246, adm0 := 'Trinidad and Tobago']
-
-#place those in need of attention aside
-c4 <- c2[no_year == 1 | no_denom == 1,]
-#write.csv(c4, file=paste0(latest_dir, '/no_denom_no_year.csv'))
-
-#need iso3 codes for R shiny and data plots
-iso3 <- fread(file='C:/Users/stearns7/OneDrive - UW Office 365/spatial_data/iso3.csv', stringsAsFactors = F)
-
-iso3 <- iso3[,c('name', 'ISO3166-1-Alpha-3', 'GAUL'), with=F]
-iso3[GAUL==202, name:='Republic of Korea']
-iso3[`ISO3166-1-Alpha-3` == 'CIV', name:="Cote d'Ivoire"]
-iso3[`ISO3166-1-Alpha-3` == 'COG', name:="Congo"]
-iso3[`ISO3166-1-Alpha-3` == 'COD', name:="Democratic Republic of the Congo"]
-iso3[`ISO3166-1-Alpha-3` == 'FSM', name:="Micronesia (Federated States of)"]
-iso3[`ISO3166-1-Alpha-3` == 'TTO', name:="Trinidad and Tobago"]
-iso3[`ISO3166-1-Alpha-3` == 'TZA', name:="Tanzania"]
-
-#match to data
-try1 <- merge(c3, iso3, by.x = 'adm0', by.y='name')
-
-try2 <- try1[,-c('country')]
-try2[,country:=`ISO3166-1-Alpha-3`]
-
-# -------------------------------------- trimming fields -------------------------------------------------
-d <- copy(try2)
-
-d1 <- d[,c('lf_uid', 'source', 'original_year', 'country', 'point', 'latitude', 'longitude', 'shapefile', 'location_code', 'N', 
-           'lf_pos', 'lf_prev', 'weight', 'cluster_id', 'year'), with=F]
-
-write.csv(d1,file = paste0(save_dir,'/lf_data_pts&poly_6.1.csv'))
-
-
-# ---------------------------------------- pts only run, 2000-2015 -----------
-e <- copy(d1)
-
-all_pts <- e[point==1,]
-
-#trim
-all_pts1 <- all_pts[,-c('point', 'shapefile', 'location_code'), with=F]
-
-write.csv(all_pts1, file = paste0(save_dir, '/pts_only_6.1.csv'))
-
-# --------------------------------------- prev loess curve --------------
-pts <- fread(file = paste0(save_dir, '/pts_only_6.1.csv'), stringsAsFactors = F)
-pts_ssa <- fread(file = 'C:/Users/stearns7/Desktop/input_data.csv', stringsAsFactors = F)
-pts_poly <- fread(file = paste0(save_dir,'/lf_data_pts&poly_5.25.csv'), stringsAsFactors = F)
-
-
-#lf prev & year -- all
-ggplot(pts, aes(x=year, y=lf_prev)) +
-  geom_point(shape=16) +
-  geom_smooth()
-
-# for SSA
-ggplot(pts_ssa, aes(x=year, y=lf_prev)) +
-  geom_point(shape=16) +
-  geom_smooth()
-
-# for pts and poly
-ggplot(pts_poly, aes(x=year, y=lf_prev)) +
-  geom_point(shape=16) +
-  geom_smooth()
-
-ssa <- pts_ssa[['country']]
-
-ptspoly_ssa <- pts_poly[country %in% ssa,]
-
-# for pts and poly in ssa
-ggplot(ptspoly_ssa, aes(x=year, y=lf_prev)) +
-  geom_point(shape=16) +
-  geom_smooth()
